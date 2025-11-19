4d55cc77f96f7ec012dbc1b901018354dfb1216c
gomesdev87 <134112503+gomesdev87@users.noreply.github.com>
2025-11-19 18:35:12 +0000

    commit  inicial

---
+++ src/main/java/br/com/gomesdev87/openfinace/OpenfinaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomesdev87.openfinace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenfinaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenfinaceApplication.class, args);
+	}
+
+}
